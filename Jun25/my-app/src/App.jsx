f8764e341013638429c7dad54e1da50fd8cbdec4
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-06-25 13:51:04 +0600

    Add initial Tailwind CSS configuration file

---
+++ Jun25/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl bg-green-400'>Hello world</h1>
+      <p className='text-lg text-blue-600'>This is a simple React app with Tailwind CSS.</p>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default App
