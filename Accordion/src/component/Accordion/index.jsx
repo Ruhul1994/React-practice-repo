c661ec808dc28aa143ad1e0fb1378e7019b06493
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-07-18 01:03:54 +0600

    feat: Implement click handling for Accordion titles to log selected item ID

--- Accordion/src/component/Accordion/index.jsx
+++ Accordion/src/component/Accordion/index.jsx
@@ -4,12 +4,16 @@ import accordionData from './data';
 
 export default function Accordion() {
     // const [selected, setSelected] = useState(null);
+
+    function handleSingleSelection(getCurrentId){
+        console.log(getCurrentId)
+    }
   return (
     <div className="wrapper">
     <div className="accordion">
         {accordionData && accordionData.length > 0 &&
         accordionData.map((item) => (
-          <div className='title' key={item.id}>
+          <div className='title' onClick={() => handleSingleSelection(item.id)} key={item.id}>
             <h3>{item.question}</h3>
             <span>+</span>
           </div>
