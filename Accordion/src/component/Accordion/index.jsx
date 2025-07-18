aa5d30413ed9fb24379f2d6d1f4156998b3d5eb4
Ruhul1994 <k.m.ruhulkuddus@gmail.com>
2025-07-18 17:31:44 +0600

    Refactor code structure for improved readability and maintainability

--- Accordion/src/component/Accordion/index.jsx
+++ Accordion/src/component/Accordion/index.jsx
@@ -1,24 +1,64 @@
-// import{ useState } from 'react';
-// import './Accordion.css'; // Assuming you have a CSS file for styling
-import accordionData from './data';
+import { useState } from "react";
+import "./styles.css"; // Assuming you have a CSS file for styling
+import accordionData from "./data";
 
 export default function Accordion() {
-    // const [selected, setSelected] = useState(null);
+  const [selected, setSelected] = useState(null);
 
-    function handleSingleSelection(getCurrentId){
-        console.log(getCurrentId)
+  const [enableMultiSelection, setEnableMultiSelection] =useState(false);
+
+  const [multiple, setMultiple] = useState([])
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+  function handleMultipleSelection(getCurrentId) {
+    let copyMultiple = [...multiple];
+    const fiendIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+    if (fiendIndexOfCurrentId === -1) {
+      copyMultiple.push(getCurrentId);
+    } else {
+      copyMultiple.splice(fiendIndexOfCurrentId, 1);
     }
+    setMultiple(copyMultiple);
+  }
+
   return (
     <div className="wrapper">
-    <div className="accordion">
-        {accordionData && accordionData.length > 0 &&
-        accordionData.map((item) => (
-          <div className='title' onClick={() => handleSingleSelection(item.id)} key={item.id}>
-            <h3>{item.question}</h3>
-            <span>+</span>
-          </div>
-        ))}
+      <button
+        style={{
+          color: enableMultiSelection ? "red" : "white"
+        }}
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+      >
+        Multiple selection
+      </button>
+      <div className="accordion">
+        {accordionData && accordionData.length > 0 ? (
+          accordionData.map((item) => (
+            <div className="item" key={item.id}>
+              <div
+                className="title"
+                onClick={enableMultiSelection ? 
+                  () => handleMultipleSelection(item.id) : 
+                  () => handleSingleSelection(item.id)}
+              >
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>
+              {
+                enableMultiSelection
+                  ? (multiple.indexOf(item.id) !== -1 && <div className="content">{item.answer}</div>)
+                  : (selected === item.id ? <div className="content">{item.answer}</div> : null)
+              }
+        
+             
+            </div>
+          ))
+        ) : (
+          <div>data is not found</div>
+        )}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
